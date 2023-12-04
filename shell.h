522042fdc896b84e0c680ad9c6f782979557bc50
kathyslate <mercyoyetunde98@gmail.com>
2023-12-04 13:28:57 -0800

    simple shell

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+#define PATH_MAX_LENGTH 4096
+#define PATH_SEPARATOR ":"
+#define PROMPT "$ "
+#define MAX_TOKENS 1024
+#define BUFFER_SIZE 1024
+
+void simple_shell()
+
+#endif
