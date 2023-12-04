b0577fd5c0bcbbf8bfdb62422cec5d1e5fa438a3
x17-green <x17.ebi@icloud.com>
2023-12-04 22:48:01 +0100

    simple shell

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SIMPLE_SHELL_
+#define _SIMPLE_SHELL_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void simple_shell(void);
+int main(void);
+
+#endif
