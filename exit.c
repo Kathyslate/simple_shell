cfbd7c15111d73e52d81203a13b4992a7144e71c
x17-green <x17.ebi@icloud.com>
2023-12-07 19:09:22 +0100

    updated env built-in file

--- exit.c
+++ exit.c
@@ -11,59 +11,80 @@
 int is_built_in(char *command);
 void execute_built_in(char *command);
 
-int main() {
-    char command[MAX_COMMAND_SIZE];
-    char *arguments[MAX_ARGUMENTS];
-    int should_run = 1;
+int main(void)
+{
+	char command[MAX_COMMAND_SIZE];
+	char *arguments[MAX_ARGUMENTS];
+	int should_run = 1;
 
-    while (should_run) {
-        printf("> ");
-        fgets(command, MAX_COMMAND_SIZE, stdin);
-        command[strcspn(command, "\n")] = '\0';
+	while (should_run)
+	{
+		printf("megnix> ");
+		fgets(command, MAX_COMMAND_SIZE, stdin);
+		command[strcspn(command, "\n")] = '\0';
 
-        if (is_built_in(command)) {
-            execute_built_in(command);
-        } else {
-            int pid = fork();
-            if (pid == 0) {
-                char *path = getenv("PATH");
-                char *token = strtok(path, ":");
-                while (token != NULL) {
-                    char filepath[1024];
-                    snprintf(filepath, sizeof(filepath), "%s/%s", token, command);
-                    if (access(filepath, F_OK) != -1) {
-                        arguments[0] = command;
-                        arguments[1] = NULL;
-                        execv(filepath, arguments);
-                        break;
-                    }
-                    token = strtok(NULL, ":");
-                }
-                if (token == NULL) {
-                    printf("command not found: %s\n", command);
-                }
-                exit(0);
-            } else if (pid < 0) {
-                printf("fork failed\n");
-            } else {
-                int status;
-                waitpid(pid, &status, 0);
-            }
-        }
-    }
+		if (is_built_in(command))
+		{
+			execute_built_in(command);
+		}
+		else
+		{
+			int pid = fork();
 
-    return 0;
+			if (pid == 0)
+			{
+				char *path = getenv("PATH");
+				char *token = strtok(path, ":");
+
+				while (token != NULL)
+				{
+					char filepath[1024];
+
+					snprintf(filepath, sizeof(filepath), "%s/%s", token, command);
+					if (access(filepath, F_OK) != -1)
+					{
+						arguments[0] = command;
+						arguments[1] = NULL;
+						execv(filepath, arguments);
+						break;
+					}
+					token = strtok(NULL, ":");
+				}
+				if (token == NULL)
+				{
+					printf("command not found: %s\n", command);
+				}
+				exit(0);
+			}
+			else if (pid < 0)
+			{
+				printf("fork failed\n");
+			}
+			else
+			{
+				int status;
+
+				waitpid(pid, &status, 0);
+			}
+		}
+	}
+
+	return (0);
 }
 
-int is_built_in(char *command) {
-    if (strcmp(command, "exit") == 0) {
-        return 1;
-    }
-    return 0;
+int is_built_in(char *command)
+{
+	if (strcmp(command, "exit") == 0)
+	{
+		return (1);
+	}
+	return (0);
 }
 
-void execute_built_in(char *command) {
-    if (strcmp(command, "exit") == 0) {
-        exit(0);
-    }
+void execute_built_in(char *command)
+{
+	if (strcmp(command, "exit") == 0)
+	{
+		exit(0);
+	}
 }
