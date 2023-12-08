e1b6f075d2416f4d6256e78240b216035f829575
kathyslate <mercyoyetunde98@gmail.com>
2023-12-08 04:34:02 -0800

    simple shell

--- exit.c
+++ exit.c
@@ -1,45 +1,89 @@
+/*
+ * File - test.c
+ * Authors - Green Ebisine and Mercy Oyetunde
+ *
+ * Description - handle arguments for the built-in exit
+ * Usage: exit status, where status is an integer used to exit the shell
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "shell.h"
 
-void exit_shell(const char *args[]) {
-    if (args[1] != NULL) {
-        fprintf(stderr, "exit: too many arguments\n");
-        return;
-    }
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_NUM_ARGUMENTS 64
 
-    exit(EXIT_SUCCESS);
-}
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: always return 0
+ */
 
-int main() {
-    char *line = NULL;
-    size_t size = 0;
+int main(int argc, char *argv[])
+{
+	char *command;
+	char *arguments[MAX_NUM_ARGUMENTS];
+	int status;
 
-    while (1) {
-        printf("mysh> ");
-        if (get_line(&line, &size, stdin) < 0) {
-            exit(EXIT_SUCCESS);
-        }
+	while (1)
+	{
+		printf("megnix: ");
+		fflush(stdout);
 
-        argc = 0;
-        args = (char **) malloc(sizeof(char *));
-        token = strtok(line, " \t\r\n");
 
-        while (token != NULL) {
-            args = (char **) realloc(args, sizeof(char *) * (argc + 2));
-            args[argc++] = token;
-            token = strtok(NULL, " \t\r\n");
-        }
+		command = (char *) malloc(MAX_COMMAND_LENGTH * sizeof(char));
+		fgets(command, MAX_COMMAND_LENGTH, stdin);
 
-        args[argc] = NULL;
+		if (strncmp(command, "exit", 4) == 0)
+		{
+			int status_code;
 
-        if (strcmp(args[0], "exit") == 0) {
-            exit_shell(args);
-        }
+			if (sscanf(command, "exit %d", &status_code) == 1)
+			{
+				free(command);
+				exit(status_code);
+			} else
+			{
+				free(command);
+				exit(0);
+			}
+		}
 
- 
-        free(args);
-    }
+		char *token = strtok(command, " \n");
+		int i = 0;
 
-    return 0;
+		while (token != NULL && i < MAX_NUM_ARGUMENTS)
+		{
+			arguments[i++] = token;
+			token = strtok(NULL, " \n");
+		}
+		arguments[i] = NULL;
+
+
+		pid_t pid = fork();
+
+		if (pid == 0)
+		{
+			execvp(arguments[0], arguments);
+			printf("Command not found: %s\n", arguments[0]);
+			exit(0);
+		} else if (pid < 0)
+		{
+			printf("Fork failed\n");
+		} else
+		{
+			do {
+				waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+
+		free(command);
+	}
+
+	return (0);
 }
