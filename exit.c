6d6b6aaec0f0348ffc975665becd7dc59da66085
kathyslate <mercyoyetunde98@gmail.com>
2023-12-13 04:25:16 -0800

    dffsss

---
+++ exit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "shell.h"
+
+int exiting(void)
+{
+        char command[MAX_LINE_LENGTH];
+
+        signal(SIGINT, exit_handler);
+
+
+        while (1)
+        {
+                printf("megnix> ");
+                fflush(stdout);
+                read_command(command);
+
+                if (strcmp(command, "exit") == 0)
+                {
+                        exit_handler(0);
+                }
+                if (execute_command(command) != 0)
+                {
+                        break;
+                }
+        }
+
+        return (0);
+}
